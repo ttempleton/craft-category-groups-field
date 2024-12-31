2f00a8c967adceab363f450704f241b40bfafc5f
Thomas Templeton <thomas.templeton.dev@gmail.com>
2024-12-31 19:55:21 +1100

    Fix ECS

--- ecs.php
+++ ecs.php
@@ -3,16 +3,15 @@
 declare(strict_types=1);
 
 use craft\ecs\SetList;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
 use Symplify\EasyCodingStandard\ValueObject\Option;
 
-return static function(ContainerConfigurator $containerConfigurator): void {
-    $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::PARALLEL, true);
-    $parameters->set(Option::PATHS, [
-        __DIR__ . '/src',
-        __FILE__,
-    ]);
-
-    $containerConfigurator->import(SetList::CRAFT_CMS_4);
+return static function(ECSConfig $ecsConfig): void {
+	$ecsConfig->parallel();
+	$ecsConfig->paths([
+		__DIR__ . '/src',
+		__FILE__,
+	]);
+	$ecsConfig->sets([SetList::CRAFT_CMS_4]);
+	$ecsConfig->indentation(Option::INDENTATION_TAB);
 };
